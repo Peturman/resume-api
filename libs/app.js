22619990a530571506f0c49d25187991285fa87e
peturman <peturman@163.com>
2017-09-21 14:10:39 +0800

    cli

---
+++ libs/app.js
@@ -0,0 +1,69 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var passport = require('passport');
+var methodOverride = require('method-override');
+var session = require("express-session");
+
+var libs = process.cwd() + '/libs/';
+require(libs + 'auth/auth');
+
+var config = require('./config');
+var log = require('./log')(module);
+var oauth2 = require('./auth/oauth2');
+
+var api = require('./routes/api');
+var users = require('./routes/users');
+var articles = require('./routes/articles');
+
+var app = express();
+
+app.use(session({ secret: "cats" }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(methodOverride());
+app.use(passport.initialize());
+app.use(passport.session());
+
+//设置跨域访问
+app.all('*', function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With');
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Credentials', 'true');
+    if (req.method == 'OPTIONS') {
+        res.send(200);
+    } else {
+        next();
+    }
+});
+
+app.use('/', api);
+app.use('/api', api);
+app.use('/api/users', users);
+app.use('/api/articles', articles);
+app.use('/api/oauth/token', oauth2.token);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next){
+    res.status(404);
+    log.debug('%s %d %s', req.method, res.statusCode, req.url);
+    res.json({ 
+    	error: 'Not found' 
+    });
+    return;
+});
+
+// error handlers
+app.use(function(err, req, res, next){
+    res.status(err.status || 500);
+    log.error('%s %d %s', req.method, res.statusCode, err.message);
+    res.json({ 
+    	error: err.message 
+    });
+    return;
+});
+
+module.exports = app;
